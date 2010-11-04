f2413126efcf5a09f322eca3b7c1d5fb3f1ce83b
Rory Hart <hartror@gmail.com>
2010-11-04 22:55:18 +1100

    Added template directory.

--- mpug_demo/live_settings.py
+++ mpug_demo/live_settings.py
@@ -79,9 +79,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'mpug_demo.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    ROOT_DIR+'templates',
 )
 
 INSTALLED_APPS = (
