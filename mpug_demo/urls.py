f58e1f95d8b5837c39f3fb0c1b1c7d9ab3c22848
Rory Hart <hartror@gmail.com>
2010-11-04 22:35:30 +1100

    Added basic urls.

--- mpug_demo/urls.py
+++ mpug_demo/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^mpug_demo/', include('mpug_demo.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns(
+    '',
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^', include('mpug_demo.demo.urls')),
 )
